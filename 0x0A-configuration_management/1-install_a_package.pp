c7b846c1b4591e44b786c6e02371f3439c52f11e
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-26 05:47:03 -0700

    task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package { 'werkzeug':
-  ensure   => '2.0.2',  # Adjust as necessary for compatibility
-  provider => 'pip3'
-}
-
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
-  require  => Package['werkzeug']
 }
