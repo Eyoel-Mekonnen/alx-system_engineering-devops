be779c21c30c5a04125c851408a44450a6c023e0
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-06-11 11:08:53 +0300

    tasks0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+
+""" Fetches users and todo from the API"""
 import requests
 import sys
 
 if __name__ == "__main__":
     id_ = int(sys.argv[1])
-    to_do_api = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id_)
+    to_do_ = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id_)
     user_api = "https://jsonplaceholder.typicode.com/users"
-    to_do_api_response = requests.get(to_do_api)
+    to_do_api_response = requests.get(to_do_)
     user_api_response = requests.get(user_api)
     to_do = to_do_api_response.json()
     user = user_api_response.json()
@@ -14,15 +16,17 @@ if __name__ == "__main__":
         if value["id"] == id_:
             employee_name = value["name"]
     number_of_completed = 0
-    total_number_of_task = 0
+    total_number_task = 0
     list_of_tasks_completed = []
     for value in to_do:
-        if value["userId"] == id_ and value["completed"] == True:
+        if value["userId"] == id_ and value["completed"] is True:
             number_of_completed = number_of_completed + 1
             list_of_tasks_completed.append(value["title"])
-        elif value["userId"] == id_ and value["completed"] == False:
-            total_number_of_task = total_number_of_task + 1
-    total_number_of_task = total_number_of_task + number_of_completed
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, number_of_completed, total_number_of_task))
+        elif value["userId"] == id_ and value["completed"] is False:
+            total_number_task = total_number_task + 1
+    total_number_task = total_number_task + number_of_completed
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+                                                          number_of_completed,
+                                                          total_number_task))
     for tasks in list_of_tasks_completed:
         print("     {}".format(tasks))
