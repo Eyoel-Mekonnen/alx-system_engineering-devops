d622153d8d56b702684f2e4b6b5b71a92eb6e1c7
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-06-28 16:51:28 +0300

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,13 +8,10 @@ def number_of_subscribers(subreddit):
     headers = {
                 "User-Agent": "linux_LTS:0x16.api.advanced:v1.0.0"
             }
-    try:
-        api_subscribers_data = requests.get(url, headers=headers, allow_redirects=False)
-        if api_subscribers_data.status_code == 200:
-            api_subscribers = api_subscribers_data.json()
-            number_of_subscribers = api_subscribers['data']['subscribers']
-            return number_of_subscribers
-        else:
-            return 0
-    except requests.RequestException as e:
+    api_subscribers_data = requests.get(url, headers=headers, allow_redirects=False)
+    if api_subscribers_data.status_code == 200:
+        api_subscribers = api_subscribers_data.json()
+        number_of_subscribers = api_subscribers['data']['subscribers']
+        return number_of_subscribers
+    else:
         return 0
